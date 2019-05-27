570114f9dd91903c8efd92cd75aa1e36cbc9e744
bookkojot <32748210+bookkojot@users.noreply.github.com>
2019-05-26 22:56:35 -0400

    Added options and code cleanup

--- do.sh
+++ do.sh
@@ -1,27 +1,42 @@
-TEXT="Какой-то текст";
+# Define here your options!
 
-convert -pointsize 70 label:"$TEXT" -bordercolor white -border 20x20 pic1.bmp
+TEXT="Какой-то текст"
+POINTSIZE=38
+BORDERS=50
+FINALSIZE="1200x1200"
+
+# Warning! All data in this directory will erased!!!
+OUTDIR="marked"
+
+# Don't touch... If you want
+
+OVERLAY=overlay.bmp
+
+echo Creating base wall
+convert -pointsize $POINTSIZE label:"$TEXT" -bordercolor white -border ${BORDERS}x${BORDERS} pic1.bmp
 convert pic1.bmp -crop 50%x100% pic2.bmp
 convert pic1.bmp -gravity west pic2-1.bmp -composite -gravity east pic2-0.bmp -composite pic3.bmp
-montage -tile 1x -geometry +0+0 pic1.bmp pic3.bmp test5.png
-montage -tile 5x -geometry +0+0 test5.png test5.png test5.png test5.png test5.png test5.png test5.png test5.png test5.png test5.png test5.png test5.png test5.png test5.png test5.png test5.png test5.png test5.png test5.png test5.png test5.png test5.png test5.png test5.png test5.png test5.png test5.png test5.png test5.png test5.png test5.png test5.png test5.png test5.png test5.png test5.png test5.png test5.png test5.png test5.png test5.png test5.png test5.png test5.png test5.png test5.png test5.png test5.png test5.png test5.png test5.png test5.png test5.png test5.png test5.png tiles.png
-convert tiles.png tiles.bmp
+montage -tile 1x -geometry +0+0 pic1.bmp pic3.bmp test5.bmp
+montage -tile 5x -geometry +0+0 test5.bmp test5.bmp test5.bmp test5.bmp test5.bmp test5.bmp test5.bmp test5.bmp test5.bmp test5.bmp test5.bmp test5.bmp test5.bmp test5.bmp test5.bmp test5.bmp test5.bmp test5.bmp test5.bmp test5.bmp test5.bmp test5.bmp test5.bmp test5.bmp test5.bmp test5.bmp test5.bmp test5.bmp test5.bmp test5.bmp test5.bmp test5.bmp test5.bmp test5.bmp test5.bmp test5.bmp test5.bmp test5.bmp test5.bmp test5.bmp test5.bmp test5.bmp test5.bmp test5.bmp test5.bmp test5.bmp test5.bmp test5.bmp test5.bmp test5.bmp test5.bmp test5.bmp test5.bmp test5.bmp test5.bmp tiles.bmp
+echo "Warping tiles..."
 for q in {1..15}; do
-echo Warping $q
+echo "Warping $q of 15"
 convert tiles.bmp -wave $((1+($RANDOM%3)))x$((120+($RANDOM%200))) -rotate 22 -trim tiles2.bmp
 mv tiles2.bmp tiles.bmp
 done
 
-convert tiles.bmp -unsharp 10x10 -resize 50% -gravity center -crop 1000x1000+0+0 +repage overlay.png
+echo "Making final overlay"
+convert tiles.bmp -unsharp 10x10 -resize 50% -gravity center -crop "$FINALSIZE"+0+0 +repage -unsharp 2x2 +level 85%,100% "$OVERLAY"
 
-OVERLAY=overlay.png
-mkdir marked
-rm marked/*
-for q in *jpg; do
-echo "processing $q"
-convert -alpha copy -size 1000x1000 plasma:fractal "$OVERLAY" -composite -swirl $(($RANDOM%20)) -implode .$(($RANDOM%5)) -wave $((10+$RANDOM%10))x$((300+$RANDOM%200)) -rotate 90 -wave $((10+($RANDOM%10)))x$((300+($RANDOM%200))) -rotate 270 -resize "1000x1000!" tmp.bmp
-convert "$q" -resize 1000x1000 -gravity center -compose multiply tmp.bmp -composite tmp2.bmp
-convert tmp2.bmp marked/"$q"
+echo "Removing all data in \"$OUTDIR\""
+rm -rf "$OUTDIR" 2> /dev/null
+mkdir -p "$OUTDIR" 2> /dev/null
 
+find . -maxdepth 1 | grep -Ei "jpe?g|png|gif"| while read q; do
+echo "Processing file $q --> ${OUTDIR}/$q"
+convert -alpha copy -size "$FINALSIZE" plasma:fractal "$OVERLAY" -composite -swirl $(($RANDOM%20)) -implode .$(($RANDOM%5)) -wave $((10+$RANDOM%10))x$((300+$RANDOM%200)) -rotate 90 -wave $((10+($RANDOM%10)))x$((300+($RANDOM%200))) -rotate 270 -resize "${FINALSIZE}!" tmp.bmp
+convert "$q" -resize "$FINALSIZE" -gravity center -compose multiply tmp.bmp -composite tmp2.bmp
+convert tmp2.bmp "$OUTDIR"/"$q"
+rm tmp2.bmp
 done
 
